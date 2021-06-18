8dbe601d1cb48ad835c5565081037e30bbdbbcb1
EndritMeziu <endrit-mziu@hotmail.com>
2021-06-18 22:48:35 +0200

    Added Xml Deserializer

---
+++ itc2021/Deserializer/XmlDeserializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace itc2021.Deserializer
+{
+    public class XmlDeserializer
+    {
+        public T DeserializeXml<T>(string input)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            var fileStream = new FileStream(input, FileMode.Open);
+
+            return (T)serializer.Deserialize(fileStream);
+        }
+    }
+}
--- itc2021/Program.cs
+++ itc2021/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.IO;
 
 namespace itc2021
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
         }
     }
 }
