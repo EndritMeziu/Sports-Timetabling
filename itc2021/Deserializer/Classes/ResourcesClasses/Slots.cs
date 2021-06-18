e0aa64850f5b208c15d03bc5430cfde2abf51794
EndritMeziu <endrit-mziu@hotmail.com>
2021-06-19 00:15:24 +0200

    Slot sub classes added

--- itc2021/Deserializer/Classes/ResourcesClasses/Slots.cs
+++ itc2021/Deserializer/Classes/ResourcesClasses/Slots.cs
@@ -10,5 +10,17 @@ namespace itc2021.Deserializer.Classes.ResourcesClasses
     [XmlRoot(ElementName = "Slots")]
     public class Slots
     {
+        [XmlElement(ElementName ="slot")]
+        public List<Slot> Slot { get; set; }
+    }
+    
+    [XmlRoot(ElementName = "slot")]
+    public class Slot
+    {
+        [XmlAttribute(AttributeName ="id")]
+        public string Id { get; set; }
+
+        [XmlAttribute(AttributeName ="name")]
+        public string Name { get; set; }
     }
 }
