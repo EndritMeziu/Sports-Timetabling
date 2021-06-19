9d528ab1a2ffc630f3c598b742adc141c536723b
EndritMeziu <endrit-mziu@hotmail.com>
2021-06-19 14:42:25 +0200

    SeparationConstraints sub classes added

--- itc2021/Deserializer/Classes/ConstraintClasses/SeparationConstraints.cs
+++ itc2021/Deserializer/Classes/ConstraintClasses/SeparationConstraints.cs
@@ -10,5 +10,27 @@ namespace itc2021.Deserializer.Classes.ConstraintClasses
     [XmlRoot(ElementName = "SepartationConstraints")]
     public class SeparationConstraints
     {
+        [XmlElement(ElementName ="SE1")]
+        public List<SE1> SE1 { get; set; }
+    }
+    
+    [XmlRoot(ElementName = "SepartationConstraints")]
+    public class SE1
+    {
+        [XmlAttribute(AttributeName ="mode1")]
+        public string Mode1 { get; set; }
+
+        [XmlAttribute(AttributeName ="min")]
+        public string Min { get; set; }
+
+        [XmlAttribute(AttributeName ="penalty")]
+        public string Penalty { get; set; }
+
+        [XmlAttribute(AttributeName ="teams")]
+        public string Teams { get; set; }
+        
+        [XmlAttribute(AttributeName ="type")]
+        public string Type { get; set; }
+
     }
 }
