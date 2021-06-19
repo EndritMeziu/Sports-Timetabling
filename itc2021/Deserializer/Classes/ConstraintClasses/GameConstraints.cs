ec2e7c2f9fb83f65757eee93f4e59a80ad6c6459
EndritMeziu <endrit-mziu@hotmail.com>
2021-06-19 14:17:22 +0200

    GameConstraints sub classes added

--- itc2021/Deserializer/Classes/ConstraintClasses/GameConstraints.cs
+++ itc2021/Deserializer/Classes/ConstraintClasses/GameConstraints.cs
@@ -10,5 +10,30 @@ namespace itc2021.Deserializer.Classes.ConstraintClasses
     [XmlRoot(ElementName = "GameConstraints")]
     public class GameConstraints
     {
+        [XmlElement(ElementName = "GA1")]
+        public List<GA1> GA1 { get; set; }
     }
+    
+    [XmlRoot(ElementName = "GA1")]
+    public class GA1
+    {
+        [XmlAttribute(AttributeName = "max")]
+        public string Max { get; set; }
+
+        [XmlAttribute(AttributeName = "meetings")]
+        public string Meetings { get; set; }
+
+        [XmlAttribute(AttributeName = "min")]
+        public string Min { get; set; }
+
+        [XmlAttribute(AttributeName = "penalty")]
+        public string Penalty { get; set; }
+        
+        [XmlAttribute(AttributeName = "slots")]
+        public string Slots { get; set; }
+        
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+    }
+    
 }
