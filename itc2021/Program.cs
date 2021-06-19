67dc8e4e3bda50fe1416dc48cb3dd4d14bf08eee
EndritMeziu <endrit-mziu@hotmail.com>
2021-06-19 20:49:02 +0200

    Added two basic constraints

--- itc2021/Program.cs
+++ itc2021/Program.cs
@@ -2,6 +2,7 @@
 using itc2021.Deserializer.Classes;
 using System;
 using System.IO;
+using Google.OrTools.LinearSolver;
 
 namespace itc2021
 {
@@ -10,8 +11,75 @@ namespace itc2021
         static void Main(string[] args)
         {
             XmlDeserializer deserializer = new XmlDeserializer();
-            var obj = deserializer.DeserializeXml<Instance>(@"C:\Users\USER\Desktop\AI Project\SportsTimeTabling\Test Instances EM\ITC2021_Test4.xml");
-            int x = 2;
+            var obj = deserializer.DeserializeXml<Instance>(@"C:\Users\USER\Desktop\AI Project\SportsTimeTabling\Test Instances EM\TestInstanceDemo.xml");
+
+            Solver solver = Solver.CreateSolver("SCIP");
+
+            int numTeams = obj.Resources.Teams.Team.Count;
+            int numSlots = obj.Resources.Slots.Slot.Count;
+            // Variables.
+            // x[i, j] is an array of 0-1 variables, which will be 1
+            Variable[,,] x = new Variable[numTeams, numTeams,numSlots];
+            for (int i = 0; i < numTeams; ++i)
+            {
+                for (int j = 0; j < numTeams; ++j)
+                {
+                    for (int k = 0; k < numSlots; ++k)
+                    {
+                        x[i, j, k] = solver.MakeIntVar(0, 1, $"worker_{i}_task_{j}");
+                    }
+                }
+            }
+
+            //Constraints
+
+            //No team plays against itself
+            for (int i = 0; i < numTeams; i++)
+            {
+                Constraint constraint = solver.MakeConstraint(0, 0, "");
+                for (int j = 0; j < numSlots; j++)
+                {
+                    constraint.SetCoefficient(x[i, i, j], 1);
+                }
+            }
+
+            //Each team plays exaclty once in each round
+            for (int k = 0; k < numSlots; k++)
+            {
+                for (int i = 0; i < numTeams; i++)
+                {
+                    Constraint constraint = solver.MakeConstraint(1, 1, "");
+                    for (int j = 0; j < numTeams; j++)
+                    {
+                        constraint.SetCoefficient(x[i, j, k], 1);
+                        constraint.SetCoefficient(x[j, i, k], 1);
+                    }
+                }
+            }
+
+            Solver.ResultStatus resultStatus = solver.Solve();
+            
+            if(resultStatus == Solver.ResultStatus.FEASIBLE || resultStatus == Solver.ResultStatus.OPTIMAL)
+            {
+                for (int k = 0; k < numSlots; k++)
+                {
+                    for (int i = 0; i < numTeams; i++)
+                    {
+                        for (int j = 0; j < numTeams; j++)
+                        {
+                            if (x[i, j, k].SolutionValue() > 0.5)
+                            {
+                                Console.WriteLine($"[Team {i} vs Team {j}] Round: {k}");
+                            }
+
+                        }
+                    }
+
+                    Console.WriteLine("----------------");
+                }
+            }
+
+
         }
     }
 }
