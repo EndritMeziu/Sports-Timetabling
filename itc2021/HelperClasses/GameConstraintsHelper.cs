00103d8450992d5b89c7872cfb6695ddbc72dc8f
EndritMeziu <endrit-mziu@hotmail.com>
2021-06-20 14:23:04 +0200

    Added GameConstraintsHelper

---
+++ itc2021/HelperClasses/GameConstraintsHelper.cs
@@ -0,0 +1,33 @@
+﻿using itc2021.Deserializer.Classes.ConstraintClasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace itc2021.HelperClasses
+{
+    public static class GameConstraintsHelper
+    {
+        public static List<string> processMeetings(GA1 gA1)
+        {
+            List<string> meetingsPairs = new List<string>();
+            if(gA1.Meetings.Contains(';'))
+            {
+                return gA1.Meetings.Split(';').ToList();
+            }
+            meetingsPairs.Add(gA1.Meetings);
+            return meetingsPairs;
+        }
+
+        public static List<string> processSlots(GA1 gA1)
+        {
+            List<string> slots = new List<string>();
+            if(gA1.Slots.Contains(';'))
+            {
+                return gA1.Slots.Split(';').ToList();
+            }
+            slots.Add(gA1.Slots);
+            return slots;
+        }
+    }
+}
